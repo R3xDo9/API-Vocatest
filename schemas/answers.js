d8a4251d5d31a181c9a35f2ee56c362d1835b054
R3xDo9 <134522931+R3xDo9@users.noreply.github.com>
2023-11-05 16:10:03 -0500

    Update answers.js

--- schemas/answers.js
+++ schemas/answers.js
@@ -1,12 +1,12 @@
 import z from 'zod'
 // Esquema para validar los datos que entran en la api
 const answerSchema = z.object({
-  'Arte y Creatividad': z.number().int().min(0).max(10),
-  'Ciencias Sociales': z.number().int().min(0).max(10),
-  'Economía, Administración y Finanzas': z.number().int().min(0).max(10),
-  'Ciencia y Tecnologia': z.number().int().min(0).max(10),
-  'Ciencias de la Salud': z.number().int().min(0).max(10),
-  'Ciencias Ecológicas y Ambientales': z.number().int().min(0).max(10)
+  'Arte y Creatividad': z.number().int().min(0).max(11),
+  'Ciencias Sociales': z.number().int().min(0).max(11),
+  'Economía, Administración y Finanzas': z.number().int().min(0).max(11),
+  'Ciencia y Tecnologia': z.number().int().min(0).max(11),
+  'Ciencias de la Salud': z.number().int().min(0).max(11),
+  'Ciencias Ecológicas y Ambientales': z.number().int().min(0).max(11)
 })
 
 export function validateAnswer (input) {
